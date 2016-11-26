7c0a37136f9f6f80f820bb27db32abf114d79050
alruiz95 <alruiz300795@gmail.com>
2016-11-26 13:48:14 -0600

    Update MasterPage

---
+++ proyecto/resources/views/layouts/master.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="Hege Refsnes">
+	<title>Bienes en Costa Rica - @yield('title')</title>
+	<link rel="stylesheet" href="assets/css/foundation.css" />
+    <script type="text/javascript" src="assets/js/vendor/modernizr.js"/>
+</head>
+<body>
+	<div class="container">
+	  <div class="row">
+	    <div class="span12">
+	      <div class="head">
+	        <div class="row-fluid">
+	            <div class="span12">
+	                <div class="span6">
+	                    <h1 class="muted">Company Name</h1>
+	                </div>
+
+	                <div class="span4 offset2" style="margin-top:15px;">
+	                    <button class="btn pull-right" type="button">Sign In</button>
+	                </div>
+	            </div>
+	        </div>
+
+	        <div class="navbar">
+	            <div class="navbar-inner">
+	                <div class="container">
+	                    <ul class="nav">
+	                        <li>
+	                            <a href="#">Inicio</a>
+	                        </li>
+
+	                        <li>
+	                            <a href="#">Buscar</a>
+	                        </li>
+
+	                        <li>
+	                            <a href="#">Registrarse</a>
+	                        </li>
+
+	                        <li>
+	                            <a href="#">Contactanos</a>
+	                        </li>
+	                    </ul>
+	                </div>
+	            </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+
+
+
+</body>
+</html>
